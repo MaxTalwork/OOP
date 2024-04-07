36a873b6590bd40a4bdfef3880ee65662939bbcd
Maxim Talancev <maxtalwork@gmail.com>
2024-04-07 19:35:00 +0300

    Функция добавления нового товара

--- master/classes.py
+++ master/classes.py
@@ -1,10 +1,39 @@
 class Product:
+    all_products = []
 
     def __init__(self, name: str, description: str, price: float, quantity: int):
         self.name = name
         self.description = description
-        self.price = price
+        self._price = price
         self.quantity = quantity
+
+        Product.all_products.append(dict(name=self.name,
+                                         description=self.description,
+                                         _price=self._price,
+                                         quantity=self.quantity))
+
+    @classmethod
+    def new_prod(cls, name, description, price, quantity):
+        new_prod_name = name
+        new_prod_description = description
+        new_prod_price = price
+        new_prod_quantity = quantity
+        return cls(new_prod_name, new_prod_description, new_prod_price, new_prod_quantity)
+
+    @property
+    def get_price(self):
+        if self.price <= 0:
+            print('Введена некорректнаяцена цена')
+            return self.price
+
+    @get_price.setter
+    def get_price(self, new_price):
+        self.price = new_price
+
+    @get_price.deleter
+    def get_price(self):
+        print('Delete price!')
+        self.price = None
 
     def __repr__(self):
         return f"{self.name}, {self.description}, {self.price}, {self.quantity}"
@@ -19,14 +48,20 @@ class Category:
     def __init__(self, name: str, description: str, products: list):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.categories_set.add(self.name)
         Category.categories_counter = len(Category.categories_set)
 
-        for x in products:
+        for x in self.__products:
             Category.products_set.add(x.name)
         Category.products_counter = len(Category.products_set)
 
+    def get_products(self):
+        prod_list_format = []
+        for prod in self.__products:
+            prod_list_format.append(f'{prod.name}, {prod._price} руб. Остаток: {prod.quantity} шт.')
+        return prod_list_format
+
     def __repr__(self):
-        return f"{self.name}, {self.description}, {self.products}"
+        return f"{self.name}, {self.description}, {self.__products}"
