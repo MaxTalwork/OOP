87b9374d33471a13529d21c41fa6f7bde61694bf
Maxim Talancev <maxtalwork@gmail.com>
2024-04-13 19:59:57 +0300

    15.1 задание 1, 2, 3

--- master/functions.py
+++ master/functions.py
@@ -1,5 +1,6 @@
 import json
-from master.classes import Category, Product
+from master.class_Category import Category
+from master.class_Product import Product
 
 
 def load_products():  # pragma: no cover
@@ -19,7 +20,8 @@ def get_category_full_list(all_products):
         pr_list = []
         for prod_data in data['products']:
             product = Product(**prod_data)
-            pr_list.append(str(product))
+            if isinstance(product, Product):
+                pr_list.append(str(product))
         category = Category(name=data['name'], description=data['description'], products=pr_list)
         category_list.append(category)
         add_new_prod_check = input(f"Хотите добавить новый товар в категорию {category.name}?"
@@ -31,9 +33,11 @@ def get_category_full_list(all_products):
     add_new_prod_cat = input(f'Хотите добавить новую категорию и товар в неё?\nВведите "yes" для подтверждения: ')
     if add_new_prod_cat == 'yes':
         pr_list = []
-        new_cat_name, new_cat_dis, pr_list = get_new_category(pr_list)
-        category = Category(new_cat_name, new_cat_dis, pr_list)
-        category_list.append(category)
+        if isinstance(add_new_prod(), Product):
+            pr_list.append(add_new_prod)
+            new_cat_name, new_cat_dis = get_new_category()
+            category = Category(new_cat_name, new_cat_dis, pr_list)
+            category_list.append(category)
     return category_list
 
 
@@ -46,13 +50,7 @@ def add_new_prod():
     return new_p
 
 
-def get_new_category(pr_list):
+def get_new_category():
     new_cat_name = input('Название категории: ')
     new_cat_dis = input('Описание категории: ')
-    new_p = Product.new_prod(
-        name=input('Название товара: '),
-        description=input('Описание товара: '),
-        price=float(input('Цена товара: ')),
-        quantity=int(input('Количество товара: ')))
-    pr_list.append(new_p)
-    return new_cat_name, new_cat_dis, pr_list
+    return new_cat_name, new_cat_dis
